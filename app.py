207df43ec2ea030d9e6f919475da0c746d28bddc
Sriram Vadlamani <sriram.vadlamani@ryte.ai>
2024-03-16 22:01:05 +0100

    Update app for 4 columns

--- app.py
+++ app.py
@@ -32,64 +32,124 @@ name_select_title = """
 st.markdown(name_select_title, unsafe_allow_html=True)
 selected_name = st.selectbox("Choose your name:", names, label_visibility="collapsed")
 
-if st.button("I bought the toileteries"):
-    current_toil_heading = "### :green[{name}] bought the toileteries".format(
-        name=selected_name
-    )
-    local_session_state["current_toileteries"] = selected_name
-    # st.markdown(current_toil_heading)
-    curr_index = names.index(selected_name)
-    next_index = curr_index + 1
-    next_index = next_index % len(names)
-    next_toil_heading = "### :green[{name}], it's your turn next!".format(
-        name=names[next_index]
-    )
-    local_session_state["next_toileteries"] = names[next_index]
-    with open("info.json", "w") as f:
-        json.dump(local_session_state, f)
-    # st.markdown(next_toil_heading)
+# Columns
+col1, col2, col3, col4 = st.columns(4, gap="large")
+with col1:
+    if st.button("I bought the dish soap"):
+        current_toil_heading = "### :green[{name}] bought the dish soap".format(
+            name=selected_name
+        )
+        local_session_state["dish_soap"]["current"] = selected_name
+        # st.markdown(current_toil_heading)
+        curr_index = names.index(selected_name)
+        next_index = curr_index + 1
+        next_index = next_index % len(names)
+        next_toil_heading = "### :green[{name}], it's your turn next!".format(
+            name=names[next_index]
+        )
+        local_session_state["dish_soap"]["next"] = names[next_index]
+        with open("info.json", "w") as f:
+            json.dump(local_session_state, f)
+        # st.markdown(next_toil_heading)
 
 
-if st.button("I changed the trash"):
-    current_turn_heading = "### :green[{name}] changed the trash".format(
-        name=selected_name
-    )
-    local_session_state["current_trash"] = selected_name
-    # st.markdown(current_turn_heading)
-    curr_index = names.index(selected_name)
-    next_index = curr_index + 1
-    next_index = next_index % len(names)
-    next_turn_heading = "### :green[{name}], it's your turn next!".format(
-        name=names[next_index]
-    )
-    local_session_state["next_trash"] = names[next_index]
-    with open("info.json", "w") as f:
-        json.dump(local_session_state, f)
-    # st.markdown(next_turn_heading)
+with col2:
+    if st.button("I changed the trash"):
+        current_turn_heading = "### :green[{name}] changed the trash".format(
+            name=selected_name
+        )
+        local_session_state["trash"]["current"] = selected_name
+        # st.markdown(current_turn_heading)
+        curr_index = names.index(selected_name)
+        next_index = curr_index + 1
+        next_index = next_index % len(names)
+        next_turn_heading = "### :green[{name}], it's your turn next!".format(
+            name=names[next_index]
+        )
+        local_session_state["trash"]["next"] = names[next_index]
+        with open("info.json", "w") as f:
+            json.dump(local_session_state, f)
+        # st.markdown(next_turn_heading)
+
+with col3:
+    if st.button("I bought the toilet paper"):
+        ctp = "### :green[{name}] bought the toilet paper".format(name=selected_name)
+        local_session_state["toilet_paper"]["current"] = selected_name
+        curr_index = names.index(selected_name)
+        next_index = curr_index + 1
+        next_index = next_index % len(names)
+        next_turn_heading = "### :green[{name}], it's your turn next!".format(
+            name=names[next_index]
+        )
+        local_session_state["toilet_paper"]["next"] = names[next_index]
+        with open("info.json", "w") as f:
+            json.dump(local_session_state, f)
 
 
-toileteries, trash = st.columns(2, gap="large")
+with col4:
+    if st.button("I bought the Handwash"):
+        ctp = "### :green[{name}] bought the Hand wash".format(name=selected_name)
+        local_session_state["hand_wash"]["current"] = selected_name
+        curr_index = names.index(selected_name)
+        next_index = curr_index + 1
+        next_index = next_index % len(names)
+        next_turn_heading = "### :green[{name}], it's your turn next!".format(
+            name=names[next_index]
+        )
+        local_session_state["hand_wash"]["next"] = names[next_index]
+        with open("info.json", "w") as f:
+            json.dump(local_session_state, f)
 
-with toileteries:
+st.divider()
+d1, d2, d3, d4 = st.columns(4, gap="large")
+
+with d1:
     if (
-        local_session_state["current_toileteries"] != ""
-        and local_session_state["next_toileteries"] != ""
+        local_session_state["dish_soap"]["current"] != ""
+        and local_session_state["dish_soap"]["next"] != ""
     ):
         st.markdown(
-            "## :green[{name}] last bought the toileteries.\n ## :green[{next_name}], it's your turn next!".format(
-                name=local_session_state["current_toileteries"],
-                next_name=local_session_state["next_toileteries"],
+            "## :green[{name}] last bought the dish soap.\n ## :green[{next_name}], it's your turn next!".format(
+                name=local_session_state["dish_soap"]["current"],
+                next_name=local_session_state["dish_soap"]["next"],
             )
         )
 
-with trash:
+
+with d2:
     if (
-        local_session_state["current_trash"] != ""
-        and local_session_state["next_trash"] != ""
+        local_session_state["trash"]["current"] != ""
+        and local_session_state["trash"]["next"] != ""
     ):
         st.markdown(
             "## :green[{name}] last changed the trash.\n ## :green[{next_name}], it's your turn next!".format(
-                name=local_session_state["current_trash"],
-                next_name=local_session_state["next_trash"],
+                name=local_session_state["trash"]["current"],
+                next_name=local_session_state["trash"]["next"],
+            )
+        )
+
+
+with d3:
+    if (
+        local_session_state["toilet_paper"]["current"] != ""
+        and local_session_state["toilet_paper"]["next"] != ""
+    ):
+        st.markdown(
+            "## :green[{name}] last bought the toilet paper.\n ## :green[{next_name}], it's your turn next!".format(
+                name=local_session_state["toilet_paper"]["current"],
+                next_name=local_session_state["toilet_paper"]["next"],
+            )
+        )
+
+
+with d4:
+    if (
+        local_session_state["hand_wash"]["current"] != ""
+        and local_session_state["hand_wash"]["next"] != ""
+    ):
+        st.markdown(
+            "## :green[{name}] last bought the hand wash.\n ## :green[{next_name}], it's your turn next!".format(
+                name=local_session_state["hand_wash"]["current"],
+                next_name=local_session_state["hand_wash"]["next"],
             )
         )
